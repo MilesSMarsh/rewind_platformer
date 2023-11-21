5e40aa7cb78352d98647b1ea50a3d70fcdde9878
Miles Marsh <msmarsh@ucsc.edu>
2023-11-20 21:57:19 -0800

    rewind time

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
+use std::time::Duration;
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+
 
 fn main(){
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, (setup_camera, setup_ground, spawn_player))
-        .add_systems(Update, (character_horizontal_movement, character_jump))
+        .add_systems(Update, (character_horizontal_movement, character_jump, store_pos))
+        .add_systems(Update, rewind.before(store_pos))
         .run()
 }
 
 #[derive(Component)]
 pub struct Player {
     pub speed: f32,
-    // pub past_positions: Array,
+    pub is_rewinding: bool,
 }
+
+#[derive(Component)]
+struct Past{
+    pub transforms: Vec<Transform>,
+    pub timer: Timer,
+}
+
 
 fn setup_camera(
     mut commands: Commands,
@@ -54,7 +63,7 @@ fn setup_ground(
 
 fn spawn_player(
     mut commands: Commands, 
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     let texture:Handle<Image>  = asset_server.load("box.png");
     
@@ -66,7 +75,14 @@ fn spawn_player(
         })
         .insert(Velocity{linvel: Vec2::new(0., 0.), angvel:0.})
         .insert(GravityScale(10.))
-        .insert(Player{speed: 300.})
+        .insert(Player{
+            speed: 300.,
+            is_rewinding: false
+        })
+        .insert(Past{
+            transforms: Vec::new(),
+            timer: Timer::new(Duration::new(0, 1000000), TimerMode::Repeating),
+        })
         .insert(SpriteBundle {
             global_transform: Transform::from_xyz(0., 100., 0.).into(),
             texture,
@@ -80,7 +96,7 @@ fn character_horizontal_movement(
     time: Res<Time>,
 ) {
     for(mut controller, player) in characters.iter_mut(){
-        if input.pressed(KeyCode::D) {
+        if input.pressed(KeyCode::D) && !player.is_rewinding{
             let right_speed = player.speed*time.delta_seconds();
             controller.translation = match controller.translation{
                 Some(mut v) => {
@@ -91,7 +107,7 @@ fn character_horizontal_movement(
                     Some(Vec2::new(right_speed, -1.0))
                 }
             }
-        } else if input.pressed(KeyCode::A) {
+        } else if input.pressed(KeyCode::A) && !player.is_rewinding{
             let left_speed = -player.speed*time.delta_seconds();
             controller.translation = match controller.translation{
                 Some(mut v) => {
@@ -122,16 +138,44 @@ fn character_jump(
 ) {
     for(player, output, mut velocity) in characters.iter_mut(){
         let movement_amount = player.speed * time.delta_seconds();
-        if output.grounded && input.pressed(KeyCode::W) {
+        if output.grounded && input.pressed(KeyCode::W) && !player.is_rewinding {
             velocity.linvel = Vec2::new(0.,movement_amount * 120.);
         }
     }
 }
 
-// fn store_pos(
-//     time: Res<Time>,
-//     player: Query<&Player>
-// ) {
+fn store_pos(
+    mut objects_with_past: Query<(&mut Past, &Transform, &Player)>,
+    
+) {
+    for (mut past, transform, player) in objects_with_past.iter_mut(){
+        
+        if past.timer.finished() && !player.is_rewinding{
+            past.transforms.push(*transform);
+            println!("{:?}",transform.translation.x);
+        }
+    }
+}
 
-// }
+fn rewind(
+    mut objects: Query<(&mut Past, &mut Player, &mut Transform)>,
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+){
+    for(mut past, mut object, mut transform) in objects.iter_mut(){
+        past.timer.tick(time.delta());
+        if input.pressed(KeyCode::S) && past.transforms.len() > 0{
+            object.is_rewinding = true;
+            if past.timer.finished(){
+                println!("rewinding {:?}", object.is_rewinding);
+                let this_transform = past.transforms.pop().unwrap();
+                transform.translation = this_transform.translation;
+                transform.rotation = this_transform.rotation;
+            }
+        }
+        else {
+            object.is_rewinding = false;
+        }
+    }
+}
 
